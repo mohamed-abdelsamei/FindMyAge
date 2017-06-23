770458876bd92e86ed937570a1754a8f0e64e09b
MOHAMED ESSAM <ENG.MOHAMED3SSAM@GMAIL.com>
2017-06-23 11:00:31 +0200

    create find my age app

---
+++ app/src/main/java/com/c0d3r/findmyage/findmyage/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.c0d3r.findmyage.findmyage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btnShowMyAgeClickEvent(view:View){
+        val YearOfBirth = editText.text.toString().toInt()
+        val CurrentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val age = CurrentYear - YearOfBirth
+        textView.text = "Your Age is $age Years"
+    }
+}
